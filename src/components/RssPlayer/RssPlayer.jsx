4c6608bd7b52e70bcc63173e6a7fff556c00a447
Jordan Jones <jjones09@live.co.ukl>
2019-06-16 10:47:07 +0100

    Fix issue with indexes

--- src/components/RssPlayer/RssPlayer.jsx
+++ src/components/RssPlayer/RssPlayer.jsx
@@ -12,6 +12,9 @@ const isValidEpisodeNumber = (num, total) => num && num > 0 && num <= total;
 const RssPlayer = props => {
     const player = useRef(null);
 
+    const startingIndex = isValidEpisodeNumber(props.selectedEpisode, episodeCount) ?
+      episodeCount - props.selectedEpisode : 0;
+
     const episodeIndex = stateManipulator.getStateManipulator(startingIndex);
     const playing = stateManipulator.getStateManipulator(false);
 
@@ -20,23 +23,13 @@ const RssPlayer = props => {
 
     const episodeCount = rssJson.items.length;
 
-    let startingIndex = 0;
-
-    if (isValidEpisodeNumber(props.selectedEpisode, episodeCount)) {
-      startingIndex = episodeCount - props.selectedEpisode;
-    }
-
-    const setCurrentEpisode = (e, i) => {
-      episodeIndex.setState(i);
-    }
-
     return (
         <div className="RssPlayer">
           {rssJson.items.map((item, index) => <PlayerItem
             key={`playerLink-${index}`}
             selected={episodeIndex.value === index}
             source={item.url}
-            onClick={e => setCurrentEpisode(e, index)}>
+            onClick={() => episodeIndex.setState(index)}>
             {`Episode ${episodeCount - index} - ${item.title}`}</PlayerItem>)}
           <Player src={rssJson.items[episodeIndex.value].url} controls={true} ref={player} />
           {/* <PlayerControls /> */}
